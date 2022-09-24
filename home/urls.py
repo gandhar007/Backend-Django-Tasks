4df6b93ecc7bc5980b5d0d34724a1b0e5b1e0dc6
gandhar007 <113832621+gandhar007@users.noreply.github.com>
2022-09-24 12:14:19 +0530

    Delete home directory

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('<pk1>/<pk2>/', views.output),
-]
